ff880216a0282ab144812bb8c4b99c82bea0565b
swophotcouple-arch <swophotcouple@gmail.com>
2026-05-28 10:17:44 +0530

    Scaffold data_fetch.py utility for NSE/BSE market data retrieval.

---
+++ backend/utils/data_fetch.py
@@ -0,0 +1,8 @@
+# NSE/BSE Data Fetch Utility
+
+def fetch_market_data(tickers, start_date, end_date):
+    """
+    Download price and volume data for tickers (NSE/BSE) between dates.
+    """
+    # TODO: Integrate with data API (yfinance/nsetools/custom)
+    pass
