f1a586f71ef1f2b0cb06c40898f3eabef1dcd729
swophotcouple-arch <swophotcouple@gmail.com>
2026-05-28 10:18:00 +0530

    Scaffold backtest.py utility for strategy backtesting.

---
+++ backend/utils/backtest.py
@@ -0,0 +1,8 @@
+# Backtest Utilities (vectorbt integration)
+
+def run_backtest(data, parameters):
+    """
+    Run vectorbt backtest on data with parameter grid. Returns metrics dict.
+    """
+    # TODO: Connect to vectorbt and compute strategy statistics
+    pass
